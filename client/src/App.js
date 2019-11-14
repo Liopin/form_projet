8c6ff361eb25ae78d10ca744c2aa29755d6e9e7a
WEB4 <WEB4@CRPBeauvoir.fr>
2019-11-14 10:48:49 +0100

    Components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ResgisteredList from './components/ResgisteredList';
+import Resgistered from './components/Resgistered';
+import './App.scss';
+
 
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Event</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Liste des inscrits <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Inscription</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <ResgisteredList/>
+      <Resgistered/>
     </div>
+
   );
+
 }
+
+
 
 export default App;
---
+++ client/src/components/ResgisteredList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ResgisteredList extends React.Component{
+    render(){
+       return(
+           <div>
+             <h1>Liste de personne</h1>
+           </div>
+       );
+    }
+}
+
+export default ResgisteredList;
