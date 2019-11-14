15ed67f06eed5eb2a2084024892596b9e1e4f59d
WEB4 <WEB4@CRPBeauvoir.fr>
2019-11-14 12:08:00 +0100

    userList added

--- client/src/components/ResgisteredList.js
+++ client/src/components/ResgisteredList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+
+
 class ResgisteredList extends React.Component{
+constructor(props){
+super(props);
+this.state = {userList : ['Lionel','Romain','Mickael']};
+}
     render(){
        return(
            <div>
              <h1>Liste de personne</h1>
+             {this.state.userList.map((user) => <p key={user}>{user}</p>)}
            </div>
        );
     }
